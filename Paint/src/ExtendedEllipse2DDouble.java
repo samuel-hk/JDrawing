a075054ad9bf0deabbc06f9c03dc9f82e9aca1cd
Samuel Yeung <samuelyeung@Samuels-MacBook-Air.local>
2015-11-19 14:48:23 -0500

    added extend ellipse class

---
+++ Paint/src/ExtendedEllipse2DDouble.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+
+public class ExtendedEllipse2DDouble extends Ellipse2D.Double
+{
+	Color fillColor;
+//	Color 
+	
+	public ExtendedEllipse2DDouble (double startX, double startY, double width, double height)
+	{
+		super(startX, startY, width, height);
+	}
+	
+} // end class ExtendedEllipse2DDouble 
